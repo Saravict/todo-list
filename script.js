853d266bf7d0036ea0665f1fada17a4b80cdc910
Saravict <victoria-sara2018@hotmail.com>
2019-02-17 23:28:50 -0300

    Use innerHTML to render the items

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var form = document.getElementById('form');
 var todoContainer = document.getElementById('todo-container');
 var todoItems = [];
-var todoCount = 3;
+var todoCount = 0;
 // function to get form values
 function getValues(event){
   event.preventDefault();
@@ -13,49 +13,33 @@ function getValues(event){
   var inputValue = input.value;
   var textareaValue = textarea.value;
   if(inputValue != '' && textareaValue != ''){
-    todoItems.push([inputValue, textareaValue])
-    form.reset()
-    render(inputValue, textareaValue)
+    todoItems.push([inputValue, textareaValue]);
+    form.reset();
+    renderAllItems();
   }
 }
 
 form.addEventListener('submit', getValues);
 
-function render(title, description){
-  todoCount = todoCount + 1;
+function render(title, description, todoCount){
+  var newHtml =
+  '<div class="list-item">' +
+    todoCount + 
+    '<div class="list-item-container">' + 
+      '<h2 class="list-item-title">' + title + '</h2>' +
+      '<p class="list-item-text">' + description + '</p>' +
+      '<div class="list-item-close">&times;</div>' +
+    '</div>' +
+    '↑<br>' +
+    '↓' +
+  '</div>';
+  todoContainer.innerHTML = todoContainer.innerHTML + newHtml;
+}
 
-  var todoItem = document.createElement('div');
-  var number = document.createTextNode('0' + todoCount);
-  todoItem.className = 'list-item';
-  todoItem.appendChild(number);
-
-  var todoItemContainer = document.createElement('div');
-  todoItemContainer.className = 'list-item-container';
-
-  var todoTitle = document.createElement('h2');
-  var title = document.createTextNode(title);
-  todoTitle.className = 'list-item-title';
-  todoTitle.appendChild(title);
-
-  var todoText = document.createElement('p');
-  var description = document.createTextNode(description);
-  todoText.className = 'list-item-text';
-  todoText.appendChild(description);
-
-  var todoClose = document.createElement('div');
-  var close = document.createTextNode('×');
-  todoClose.className = 'list-item-close';
-  todoClose.appendChild(close);
-
-  todoItemContainer.appendChild(todoTitle);
-  todoItemContainer.appendChild(todoText);
-  todoItemContainer.appendChild(todoClose);
-
-  todoItem.appendChild(todoItemContainer);
-  var arrowUp = document.createTextNode('↑\n');
-  var arrowDown = document.createTextNode('↓');
-  todoItem.appendChild(arrowUp);
-  todoItem.appendChild(arrowDown);
-
-  todoContainer.appendChild(todoItem);
+function renderAllItems(){
+  todoContainer.innerHTML = '<h1 class="title">LIST</h1>';
+  for(var index = 0; index < todoItems.length; index = index + 1){
+    var current = todoItems[index];
+    render(current[0], current[1], index + 1);
+  }
 }
